36871337e55d82e1b73386fa32b761583091e3ce
Diogo <diogo.correia99@ua.pt>
2021-03-11 00:59:09 +0000

    fixed script run in foreground and improved preformance even more; increased the range of tags analysed for the highlighting

--- foreground.js
+++ foreground.js
@@ -1,22 +1,5 @@
-for (const tag of ["p", "h1", "h2", "h3", "h4", "h5", "a", "div", "span"]) {
-	const objectsWithText = Array.from(document.getElementsByTagName(tag))
-									.filter(object => /\a/.test(object.textContent))
-										.filter(object => object.children.length === 0);
-	for (const obj of objectsWithText) {
-		objHTML = obj.innerHTML;
-		newInnerHTML = "";
-		let isTag = false;
-		for (let i=0; i<objHTML.length; i++) {
-			const currentChar = objHTML.charAt(i);
-
-			if (currentChar === '<') isTag = true;
-			if (currentChar === '>') isTag = false;
-
-			if (!isTag && currentChar === "a")
-				newInnerHTML+="<span class='highlighted'>"+currentChar+"</span>";
-			else
-				newInnerHTML+=currentChar;
-		}
-		obj.innerHTML = newInnerHTML;
-	}
+for (const obj of Array.from(document.getElementsByTagName("*"))
+							.filter(object => object.children.length === 0 && /\a/.test(object.textContent) && !["html", "head", "title", "style", "link", "meta", "script", "noscript"].includes(object.localName))) {
+	// find all wanted chars, and put them within a span with a class
+	obj.innerHTML = obj.innerText.replace(/a/g, "<span class='highlighted'>a</span>");
 }
