fbb948c8ff6ce865116459131bea94c7b6ef803c
Diogo <diogo.correia99@ua.pt>
2021-03-12 21:01:08 +0000

    changed highlight script to prevent an element from being highlighted more than once (spans, inside spans, like a matryoshka)

--- background.js
+++ background.js
@@ -4,48 +4,36 @@ const urlChecker = new RegExp("^(chrome||devtools)(-[a-zA-Z0-9]+)?:\/\/");
 // check if tab url is not any type of chrome:// or chrome-___:// or devtools:// with regex
 const canInject = tabInfo => (tabInfo.url && !urlChecker.test(tabInfo.url)) || (tabInfo.pendingUrl && !urlChecker.test(tabInfo.pendingUrl));
 
-const multipleScriptExecuter = scriptsInfo => {
-	for (const script of scriptsInfo) {
-		if (script.result)
-			tabs.executeScript(script.tabId, {file: script.name}, () => console.log(script.result));
-		else
-			tabs.executeScript(script.tabId, {file: script.name});
-	}
-}
+// const multipleScriptExecuter = scriptsInfo => {
+// 	for (const script of scriptsInfo) {
+// 		if (script.result)
+// 			tabs.executeScript(script.tabId, {file: script.name}, () => console.log(script.result));
+// 		else
+// 			tabs.executeScript(script.tabId, {file: script.name});
+// 	}
+// }
 
 tabs.onCreated.addListener(tab => {
 	tabs.get(tab.id, tabInfo => {
 		if (canInject(tabInfo)) {
-			multipleScriptExecuter([{name: 'highlight.js', result: "Higlighting...", tabId: null}]);
+			tabs.executeScript(null, {file: 'highlight.js'}, () => console.log("Higlighting..."));
 		}
 	});
 });
 
-tabs.onActivated.addListener(tab => {
-	tabs.get(tab.tabId, tabInfo => {
-		if (canInject(tabInfo)) {
-			multipleScriptExecuter([{name: 'highlight.js', result: "Higlighting...", tabId: null}]);
-		}
-	});
-});
-
-const getDomainAsRegex = url => {
-	// if it is chrome: or chrome-__:
-	if (/^chrome(-[a-zA-Z0-9]+)?:/.test(url))
-		return /^chrome(-[a-zA-Z0-9]+)?:/;
+// const getDomainAsRegex = url => {
+// 	// if it is chrome: or chrome-__:
+// 	if (/^chrome(-[a-zA-Z0-9]+)?:/.test(url))
+// 		return /^chrome(-[a-zA-Z0-9]+)?:/;
 	
-	// return domain as www\.domain\.xxx
-	return url.split("://")[1].split("/")[0].toString().replace(/\./g, "\\.");
-}
+// 	// return domain as www\.domain\.xxx
+// 	return url.split("://")[1].split("/")[0].toString().replace(/\./g, "\\.");
+// }
 
 tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
 	if (canInject(tabInfo)) {
-		if (changeInfo.title) {
-			// if different page of the same site
-			// if (changeInfo.url && new RegExp(getDomainAsRegex(changeInfo.url)).test(lastUrl)) {
-
-			// }
-			multipleScriptExecuter([{name: 'highlight.js', result: "Higlighting...", tabId: null}]);
+		if (changeInfo.status === "complete") {
+			tabs.executeScript(null, {file: 'highlight.js'}, () => console.log("Higlighting..."));
 			tabs.insertCSS(null, {file: 'styles.css'});
 		}
 
@@ -53,7 +41,13 @@ tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	// message from highlight script asking for the setup values
 	if (request.highlighting === "values_request") {
-		sendResponse({functionDelay: "2000", values: ["a","e","i","o","u"], unwantedTags: ["html", "body", "head", "title", "style", "link", "meta", "script", "noscript", "img", "svg"]});
+		sendResponse({
+						functionDelay: "2000", 
+						values: ["a","e","i","o","u"],
+						unwantedTags: ["html", "body", "head", "title", "style", "link", "meta", "script", "noscript", "img", "svg"],
+						highlightingClass: "wkhighlighter_highlighted"
+					});
 	}
 });
--- highlight.js
+++ highlight.js
@@ -2,6 +2,8 @@
 // 	functionDelay: delay of the setTimout function
 // 	value: value that is going to be highlighted
 chrome.runtime.sendMessage({highlighting: "values_request"}, response => {
+	const className = response.highlightingClass;
+
 	const textChildNodes = obj => Array.from(obj.childNodes).filter(node => node.nodeName === "#text");
 
 	// replace a matching regex in a text node with a document element, preserving everything else, even other 
@@ -19,14 +21,28 @@ chrome.runtime.sendMessage({highlighting: "values_request"}, response => {
 		}
 	}
 
+	const hasDirectChildHighlighted = node => {
+		for (const child of node.childNodes) {
+			if (child.classList && Array.from(child.classList).includes(className))
+				return true;
+		}
+		return false;
+	}
+
 	if (response.functionDelay) {
 		setTimeout(() => {
+			// filter all tag elements of the document that:
+			//		- haven't yet been highlighted;
+			//		- have child nodes that are text nodes;
+			//		- aren't any of the unwanted tags (html, head, etc...)
+			let filteredNodes = Array.from(document.getElementsByTagName("*")).filter(object => !(hasDirectChildHighlighted(object) || Array.from(object.classList).includes(className))).filter(object => textChildNodes(object).length > 0).filter(object => !response.unwantedTags.includes(object.localName));
 			for (const value of response.values) {
 				const span = document.createElement("span");
-				span.className = "highlighted";
+				span.className = className;
 				span.appendChild(document.createTextNode(value));
-				// filter all tag elements of the document and then chall replaceWithElem() on each element
-				Array.from(document.getElementsByTagName("*")).filter(object => textChildNodes(object).length > 0).filter(object => new RegExp(value).test(object.textContent)).filter(object => !response.unwantedTags.includes(object.localName)).forEach(obj => replaceWithElem(obj, new RegExp(value, "g"), span));
+				// filter the tag elements again for those that have text content equal to the preset regex value;
+				// iterate the filtered tag elements and call replaceWithElem each time
+				filteredNodes.filter(object => new RegExp(value).test(object.textContent)).forEach(node => replaceWithElem(node, new RegExp(value, "g"), span));
 			}
 		},response.functionDelay);
 	}
