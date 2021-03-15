8be3e01d4bb0d5c6932325e6327192ff8539b67b
Diogo <diogo.correia99@ua.pt>
2021-03-15 01:31:09 +0000

    put all the scripts related to the details popup in the file details-popup.js

--- details-popup.js
+++ details-popup.js
@@ -1,11 +1,41 @@
 document.addEventListener("mouseover", e => {
 	const createPopup = () => {
-		const fragment = document.createDocumentFragment();
-		
+		const div = document.createElement("div");
+		div.className = "wkhighlighter_rightOverFlowPopup wkhighlighter_detailsPopup";
+		document.body.appendChild(div);
+		setTimeout(() => document.getElementsByClassName("wkhighlighter_detailsPopup")[0].classList.remove("wkhighlighter_rightOverFlowPopup"), 20);
 	}
 	
 	const node = e.target;
-	if (node.className === "wkhighlighter_highlighted") {
-		createPopup();
+
+	if (node.classList.contains("wkhighlighter_highlighted")) {
+		if (document.getElementsByClassName("wkhighlighter_detailsPopup").length < 1) {
+			createPopup();
+		}
+	}
+
+	if (node.classList.contains("wkhighlighter_detailsPopup")) {
+		node.classList.add("wkhighlighter_focusPopup");
+	}
+});
+
+document.addEventListener("mouseout", e => {
+	const node = e.target;
+
+	if (node.classList.contains("wkhighlighter_detailsPopup")) {
+		node.classList.remove("wkhighlighter_focusPopup");
+	}
+});
+
+document.addEventListener("click", e => {
+	const node = e.target;
+	
+	if (document.getElementsByClassName("wkhighlighter_detailsPopup").length > 0 && !node.classList.contains("wkhighlighter_detailsPopup")) {
+		const popup = document.getElementsByClassName("wkhighlighter_detailsPopup")[0];
+		popup.classList.add("wkhighlighter_rightOverFlowPopup");
+		setTimeout(() => {
+			if (popup)
+				document.body.removeChild(popup)
+		, 200});
 	}
 });
--- highlight.js
+++ highlight.js
@@ -37,11 +37,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 		const tagFilteringConditions = tag => !unwantedTags.includes(tag.localName) && textChildNodes(tag).length > 0 && !(hasDirectChildHighlighted(tag, highlightingClass) || Array.from(tag.classList).includes(highlightingClass));
 
-		const highlighter = (delay, values, className, allWantedTags) => {
+		const highlighter = (delay, values, className, allTags) => {
 			setTimeout(() => {
 				const kanjiRegex = new RegExp(`[${values.join('')}]`, "g");
 				// check if there is any character to be highlighted
-				const nodesToBeHighlighted = allWantedTags.filter(tag => kanjiRegex.test(tag.textContent));
+				const nodesToBeHighlighted = allTags.filter(tag => kanjiRegex.test(tag.textContent));
 				if (nodesToBeHighlighted.length > 0) {
 					const span = document.createElement("span");
 					span.className = className;
@@ -57,11 +57,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		// continuously check for the number of elements in the page
 		// if that number updates, then run highlighter again
 		const highlightUpdate = setInterval(() => {
-			const allWantedTags = Array.from(document.getElementsByTagName("*"));
-			nmrElements = allWantedTags.length;
+			const allTags = Array.from(document.getElementsByTagName("*"));
+			nmrElements = allTags.length;
 			if (nmrElements !== lastNmrElements) {
 				lastNmrElements = nmrElements;
-				highlighter(20, values, highlightingClass, allWantedTags);
+				highlighter(20, values, highlightingClass, allTags);
 			}
 		}, 3000);
 
@@ -69,10 +69,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	}
 
 	// if a key was pressed, then stop the highlight update
-	if (request.key === "down") {
-		console.log("Key pressed. Highlight update stopped! Waiting for next page update to start again...");
+	if (request.key === "down")
 		clearInterval(request.intervalFunction);
-	}
 });
 
 // message to the background saying a key was pressed
@@ -80,43 +78,4 @@ document.addEventListener("keydown", e => {
 	// if the key is not a modifier
 	if (!e.getModifierState(e.key))
 		chrome.runtime.sendMessage({key: "down"});
-});
-
-document.addEventListener("mouseover", e => {
-	const createPopup = () => {
-		const div = document.createElement("div");
-		div.className = "wkhighlighter_rightOverFlowPopup wkhighlighter_detailsPopup";
-		document.body.appendChild(div);
-		setTimeout(() => document.getElementsByClassName("wkhighlighter_detailsPopup")[0].classList.remove("wkhighlighter_rightOverFlowPopup"), 20);
-	}
-	
-	const node = e.target;
-
-	if (node.classList.contains("wkhighlighter_highlighted")) {
-		if (document.getElementsByClassName("wkhighlighter_detailsPopup").length < 1) {
-			createPopup();
-		}
-	}
-
-	if (node.classList.contains("wkhighlighter_detailsPopup")) {
-		node.classList.add("wkhighlighter_focusPopup");
-	}
-});
-
-document.addEventListener("mouseout", e => {
-	const node = e.target;
-
-	if (node.classList.contains("wkhighlighter_detailsPopup")) {
-		node.classList.remove("wkhighlighter_focusPopup");
-	}
-});
-
-document.addEventListener("click", e => {
-	const node = e.target;
-	
-	if (document.getElementsByClassName("wkhighlighter_detailsPopup").length > 0 && !node.classList.contains("wkhighlighter_detailsPopup")) {
-		const popup = document.getElementsByClassName("wkhighlighter_detailsPopup")[0];
-		popup.classList.add("wkhighlighter_rightOverFlowPopup");
-		setTimeout(() => document.body.removeChild(popup), 200);
-	}
 });
